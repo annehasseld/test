15bdf35a63dbe624b612cb8ac175cef6c00717ed
Anne Hasseld <annehasseld@hartlandschools.us>
2019-08-22 16:23:17 -0400

    added test two and third test files
    added two files that I created injGrasp

---
+++ THIRD TEST.java
@@ -0,0 +1,7 @@
+public class THIRDTEST {
+    public static void main (String [] args) {
+        System.out.println ("Hello, world! TEST THREE");
+        System.out.println ("This is FUN! TEST THREE");
+
+    }
+}   
---
+++ testTWO.java
@@ -0,0 +1,7 @@
+public class testTWO {
+    public static void main (String [] args) {
+        System.out.println ("Hello, world! TEST TWO");
+        System.out.println ("This is FUN! TEST TWO");
+
+    }
+}   
